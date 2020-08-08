b9d54bf310ff0b9cd1c6992683a9e1e198b0347d
neto <garibaldi.neto@olist.com>
2020-08-08 17:35:22 -0300

    Add Take and Release Algorithm

---
+++ take_and_release.py
@@ -0,0 +1,24 @@
+"""
+A implementation considering the possibility to take an object and also
+return it later if another one more valuable is found.
+"""
+
+
+def knapsack(W, wt, val, n):
+    items_taken = []  # index to the items taken
+
+    for i in range(n + 1):
+        if wt[i - 1] <= W:  # if there is enough space yet, take the object
+            W -= wt[i - 1]
+            items_taken.append(i)
+            continue
+
+        for t in range(len(items_taken)):
+            # If new item found is more valuable and has the same weight
+            # Take it
+            if wt[i - 1] <= wt[t - 1] and val[i - 1] > val[t - 1]:
+                items_taken.pop(t)
+                items_taken.append(i)
+                break
+
+    return sum([val[i] for i in items_taken])
